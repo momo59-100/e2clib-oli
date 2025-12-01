684b2bdf995cfb1bbe99859d09a4acde60bcdc0b
momo59-100 (remplacer par pseudo utilisé pour le compte gitHub) <ennasrimohammed212@gmail.com (remplacer par adresse mail utilisz pour le compte gitHub)>
2025-12-01 16:38:40 +0100

    Ajout js

---
+++ Javascript/test.js
@@ -0,0 +1,7 @@
+let firstname;
+let lastname;
+
+firstname = prompt("quel est votre prénom")
+lastname = prompt("quel est votre nom de famille")
+
+alert(`Bonjour ${firstname} ${lastname}, comment vas tu?`)
